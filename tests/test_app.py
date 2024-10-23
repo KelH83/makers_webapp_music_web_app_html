cf2430b14c1302cf6407637aea925028e55a96ad
Kelly <kellyhowes83@gmail.com>
2024-10-23 09:29:24 +0100

    Added tests for post alnum and artist

--- tests/test_app.py
+++ tests/test_app.py
@@ -12,9 +12,13 @@ def test_get_single_album_loads(page, test_web_address):
     h1_tag = page.locator("h1")
     expect(h1_tag).to_have_text("Vulgar display of power")
 
-# def test_post_album(web_client):
-#     response = web_client.post('/albums', data={'title':'Voyage','release_year':'2022','artist_id':'2'})
-#     assert response.status_code == 200
+def test_post_album(page, test_web_address):
+    page.goto(f"http://{test_web_address}/albums/new")
+    expect(page).to_have_title('New Album')
+    h1_tag = page.locator("h1")
+    expect(h1_tag).to_have_text("New Album")
+    text_box = page.get_by_role("textbox", name="title")
+    expect(text_box).to_be_editable()
 
 def test_get_artists_loads(page, test_web_address):
     page.goto(f"http://{test_web_address}/artists")
@@ -28,6 +32,10 @@ def test_get_single_artist_loads(page, test_web_address):
     h1_tag = page.locator("h1")
     expect(h1_tag).to_have_text("Korn")
 
-def test_post_artist(web_client):
-    response = web_client.post('/artists', data={'name':'Wild nothing','genre':'Indie'})
-    assert response.status_code == 200
+def test_post_artist(page, test_web_address):
+    page.goto(f"http://{test_web_address}/artists/new")
+    expect(page).to_have_title('New Artist')
+    h1_tag = page.locator("h1")
+    expect(h1_tag).to_have_text("New Artist")
+    text_box = page.get_by_role("textbox", name="name")
+    expect(text_box).to_be_editable()
